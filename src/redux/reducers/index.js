aeb14af59ecc651ec8b1dc8c1cf64def97c4cca2
Vika <queenviktoriyphp@gmail.com>
2021-09-16 12:53:11 +0300

    fetch to server put

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,3 +1,16 @@
-export default function reducer(state = {}, action) {
-  return state;
+const initialState = {
+  people: [],
+};
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    case "SET_PEOPLE": {
+      return {
+        ...state,
+        people: [...state.people, ...action.payload],
+      };
+    }
+
+    default:
+      return state;
+  }
 }
--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -1,11 +1,18 @@
-import { takeLeading } from "redux-saga/effects";
+import { put, takeLeading } from "redux-saga/effects";
 const wait = (t) =>
   new Promise((resolve) => {
     setTimeout(resolve, t);
   });
+async function getPeople() {
+  const request = await fetch("https://swapi.dev/api/people");
+  const data = await request.json();
+
+  return data;
+}
 export function* workerSaga() {
-  yield wait(1000);
-  console.log("click from saga");
+  const data = yield getPeople();
+  console.log(data);
+  yield put({ type: "SET_PEOPLE", payload: data.results });
 }
 export function* watchClickSaga() {
   //   while (true) {
