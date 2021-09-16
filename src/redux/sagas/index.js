28c412cd3d99dfeabac5446065749019dd92c3d9
Vika <queenviktoriyphp@gmail.com>
2021-09-16 12:40:10 +0300

    takeLeading()

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -1,12 +1,18 @@
-import { take } from "redux-saga/effects";
+import { takeLeading } from "redux-saga/effects";
+const wait = (t) =>
+  new Promise((resolve) => {
+    setTimeout(resolve, t);
+  });
 export function* workerSaga() {
+  yield wait(1000);
   console.log("click from saga");
 }
 export function* watchClickSaga() {
-  while (true) {
-    yield take("CLICK");
-    yield workerSaga();
-  }
+  //   while (true) {
+  //     yield take("CLICK");
+  //     yield workerSaga();
+  //   }
+  yield takeLeading("CLICK", workerSaga);
 }
 export default function* rootSaga() {
   yield watchClickSaga();
