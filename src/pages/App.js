c72111e8fa25cf70f033ef82330200a7e93c64c9
Vika <queenviktoriyphp@gmail.com>
2021-09-17 10:45:15 +0300

    table with data

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,10 +1,69 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { selectPeople } from "../redux/reducers/people/selectors";
 
 function App() {
-  // const store = useSelector((store) => store);
+  const people = useSelector(selectPeople);
   // const dispatch = useDispatch();
-  // console.log(store);
-  return <div className="App">Redux Saga tutorial </div>;
+  console.log(people);
+  return (
+    <>
+      {" "}
+      <h1 className="App">Star Wars People</h1>
+      {people.loading ? (
+        <div className="preloader-wrapper active ">
+          <div className="spinner-layer spinner-red-only">
+            <div className="circle-clipper left">
+              <div className="circle"></div>
+            </div>
+            <div className="gap-patch">
+              <div className="circle"></div>
+            </div>
+            <div className="circle-clipper right">
+              <div className="circle"></div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <table className="highlight centered responsive-table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Birth year</th>
+              <th>Eye color</th>
+              <th>Gender</th>
+              <th>Hair color</th>
+              <th>Height</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {people?.data?.results.map((character) => {
+              const {
+                name,
+                birth_year,
+                eye_color,
+                gender,
+                hair_color,
+                height,
+                created,
+              } = character;
+              return (
+                <tr key={created}>
+                  <td>{name}</td>
+                  <td>{birth_year}</td>
+                  <td>{eye_color}</td>
+                  <td>{gender}</td>
+                  <td>{hair_color} </td>
+                  <td>{height}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
+    </>
+  );
 }
 
 export default App;
--- src/redux/sagas/people/index.js
+++ src/redux/sagas/people/index.js
@@ -9,6 +9,7 @@ import {
   takeEvery,
 } from "redux-saga/effects";
 import { LOAD_USERS, LOAD_USERS_SUCCESS } from "../../reducers/people/actions";
+import { selectPeople } from "../../reducers/people/selectors";
 //worker
 export function* loadPeopleDetails() {}
 //worker
@@ -29,7 +30,7 @@ export function* loadUsersOnRouteEnter() {
   while (true) {
     const action = yield take(LOCATION_CHANGE);
     if (action.payload.location.pathname === "/") {
-      const state = yield select((s) => s.people);
+      const state = yield select(selectPeople);
       const { page, search } = state;
       yield put({
         type: LOAD_USERS,
